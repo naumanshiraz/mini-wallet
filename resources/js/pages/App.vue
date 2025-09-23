4396c26d512425858f51204b82de491c42a202a1
Nauman Shiraz <nauman_shiraz@hotmail.com>
2025-09-23 16:30:30 +0500

    first commit

---
+++ resources/js/pages/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="p-4 text-center">
+    <h1 class="text-2xl font-bold text-blue-600">Hello from Vue 3 + Laravel 🚀</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel + Vue 3</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="antialiased">
+<!-- Vue will mount here -->
+<div id="app"></div>
+</body>
+</html>
