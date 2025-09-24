3c32950d93cfde606b8b550353c537d6c9090360
Nauman Shiraz <nauman_shiraz@hotmail.com>
2025-09-24 14:47:20 +0500

    FeatureFix: created transaction controller, created form request to validation transaction input & removed meta column (extra field) from migration

--- database/migrations/2025_09_24_083034_create_transactions_table.php
+++ database/migrations/2025_09_24_083034_create_transactions_table.php
@@ -13,15 +13,13 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
           $table->id();
-          $table->timestamps();
-
           $table->unsignedBigInteger('sender_id')->index();
           $table->unsignedBigInteger('receiver_id')->index();
           $table->decimal('amount', 18, 2);
           $table->decimal('commission_fee', 18, 2);
           $table->decimal('sender_balance_after', 18, 2);
           $table->decimal('receiver_balance_after', 18, 2);
-          $table->text('meta')->nullable();
+          $table->timestamps();
 
           $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
           $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
