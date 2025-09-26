ee2fecdbdd1053a7266fcce10243a695c27cd518
Nauman Shiraz <nauman_shiraz@hotmail.com>
2025-09-26 17:51:31 +0500

    FeatureFix: created authenticate middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
       $middleware->alias([
         'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,
       ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
